4ebf70ac8697bb2380d170a0a94d1b525da378c2
Yashwanth sure <yaswanthsure2004@gmail.com>
2024-08-15 11:45:54 +0530

    thirdcommit

--- additions/stack.java
+++ additions/stack.java
@@ -4,8 +4,8 @@ public class stack {
     public static void main(String args[])
     {
      Stack<Integer>yash=new Stack<Integer>();
-     yash.push(40);
-     yash.push(70);
+     yash.push(1);
+     yash.push(2);
      yash.push(3);
      yash.push(4);
      //System.out.println(yash);
