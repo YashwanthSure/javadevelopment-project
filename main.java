31b1bf18b4465e75c0d90137283297d2f15444b3
Yashwanth sure <yaswanthsure2004@gmail.com>
2024-08-28 14:14:19 +0530

    another one

---
+++ main.java
@@ -0,0 +1,10 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        // Creating an object of the Car class
+        Car myCar = new Car("Red", "Tesla Model S");
+
+        // Accessing the method of the object
+        myCar.displayInfo();
+    }
+}
