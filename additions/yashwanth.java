46e402c33c4c359371a676ea258e5e69ec37f23a
Yashwanth sure <yaswanthsure2004@gmail.com>
2024-09-04 12:32:22 +0530

    todays change

--- additions/yashwanth.java
+++ additions/yashwanth.java
@@ -1,17 +1,18 @@
 package additions;
 import java.util.Scanner;
-public class yashwanth{
-    public static void main(String args[])
-    {
+
+public class yashwanth {
+
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        String lucky=sc.nextLine();
         int yash=sc.nextInt();
-        int nar=sc.nextInt();
-        System.out.println(yash+nar);
-
-
+        double shahid=sc.nextDouble();
+        System.out.println("int"+yash);
+        System.out.println("double"+shahid);
+        System.out.println("string"+lucky);
         
-
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     }
-
-
 }
